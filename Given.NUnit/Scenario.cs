6712d35aa615ad13b39de60056b78651922b104c
nathan gonzalez <nathan@gonzalez.com>
2013-02-18 11:49:41 -0600

    removed dependency on nunit.core, late assembly binding to account for varying versions

--- Given.NUnit/Scenario.cs
+++ Given.NUnit/Scenario.cs
@@ -1,7 +1,9 @@
-﻿using Given.Common;
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Given.Common;
 using NUnit.Framework;
 using TestState = Given.Common.TestState;
-using Core = NUnit.Core;
 
 namespace Given.NUnit
 {
@@ -43,9 +45,35 @@ namespace Given.NUnit
                     state = TestState.Unknown;
                     break;
             }
-            
-            _testStateManager.SetThenState(context.Test.Name, state, Core.TestExecutionContext.CurrentContext.CurrentResult.Message);
 
+            var message = GetTestExecutionMessage();
+
+            _testStateManager.SetThenState(context.Test.Name, state, message == null ? string.Empty : message.ToString());
+
+        }
+
+        static object GetTestExecutionMessage()
+        {
+            try
+            {
+                var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+                var nunitCore = assemblies.First(x => x.GetName().Name == "nunit.core");
+                var nunitCoreInterfaces = assemblies.First(x => x.GetName().Name == "nunit.core.interfaces");
+                var testExecutionContextType = nunitCore.GetType("NUnit.Core.TestExecutionContext");
+                var testResultType = nunitCoreInterfaces.GetType("NUnit.Core.TestResult");
+                var currentContextMethod = testExecutionContextType.GetProperty("CurrentContext", BindingFlags.Public | BindingFlags.Static).GetGetMethod();
+                var testResultMethod = testExecutionContextType.GetProperty("CurrentResult", BindingFlags.Public | BindingFlags.Instance).GetGetMethod();
+                var messageMethod = testResultType.GetProperty("Message", BindingFlags.Public | BindingFlags.Instance).GetGetMethod();
+
+                var testExecutionContext = currentContextMethod.Invoke(null, null);
+                var testResult = testResultMethod.Invoke(testExecutionContext, null);
+                var message = messageMethod.Invoke(testResult, null);
+                return message;
+            }
+            catch (Exception)
+            {
+                return string.Empty;
+            }
         }
     }
 }
