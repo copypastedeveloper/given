4eb091a0477e80abf4847e943a6ebb28e4242c6f
Karson Alford <Karson.Alford@gmail.com>
2014-05-04 06:52:35 -0500

    Changes to retrieval of ConcreteTypes require scanned types to be public.

--- Given.Common/DefaultReportConfiguration.cs
+++ Given.Common/DefaultReportConfiguration.cs
@@ -4,7 +4,7 @@ using System.Linq;
 
 namespace Given.Common
 {
-    internal class DefaultReportConfiguration : IReportConfiguration
+    public class DefaultReportConfiguration : IReportConfiguration
     {
         public object TestRunId { get { return Guid.NewGuid(); } }
         public string AssemblyHeader { get { return TestRunManager.CurrentTestRun.GetStories().SelectMany(x => x.Tests).First().Type.Assembly.GetName().Name; } }
